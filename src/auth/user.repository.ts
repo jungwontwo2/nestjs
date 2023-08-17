776e685129dfdc10d650d3aed745869e5f06bc2e
jungwon <jungwontwo2@naver.com>
2023-08-18 08:59:15 +0900

    비밀번호 암호화 하기소스 코드 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,25 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth=credential.dto";
-
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs'
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
     async createUser(authCredentialsDto:AuthCredentialsDto):Promise<void>{
         const {username,password}=authCredentialsDto;
-        const user = this.create({username:username,password:password});
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password,salt);
+        const user = this.create({username:username,password:hashedPassword});
+
+        try {
         await this.save(user);
+        } catch (error) {
+            if(error.code==='23505'){
+                throw new ConflictException('Existing username');
+            }else{
+                throw new InternalServerErrorException();
+            }
+            //console.log('error',error);
+        }
     }
 }
